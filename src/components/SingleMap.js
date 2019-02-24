e0b15540b77c4bbfe0ffdb8a201c9121fe6a9e3c
Samuel <bouaroua.samuel@gmail.com>
2019-02-24 21:14:52 +0100

    responsive map

--- src/components/SingleMap.js
+++ src/components/SingleMap.js
@@ -3,14 +3,13 @@ import React, { Component } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ReactMapGL from "react-map-gl";
-import "./SingleMap.scss";
 class SingleMap extends Component {
   constructor(props) {
     super(props);
     this.state = {
       viewport: {
-        width: "75%",
         height: window.innerHeight - 56,
+        // get navbar current height instead
         latitude: 48.85341,
         longitude: 2.3488,
         zoom: 9.5,
@@ -20,26 +19,22 @@ class SingleMap extends Component {
     };
   }
 
-  // fit map width to column siz
-  componentDidMount() {
-    let currentWidth = document.querySelector("#col-map").offsetWidth;
-    let foo = document.getElementById("col-map").offsetWidth;
-    console.log(foo, currentWidth);
-    // gives whole document width instead of the single col
-  }
-  // possible alternative
-  // https://github.com/uber/react-map-gl/issues/604#issuecomment-435292147
+  // adapt viewport to col width
+  _onViewportChange = viewport => this.setState({ viewport });
 
   render() {
+    const { viewport } = this.state;
+
     return (
       <Row className="no-gutters">
         <Col lg={3}> FILTERS HERE</Col>
-        <Col xs={9} id="col-map">
+        <Col sm={12} md={9}>
           {/* // REFACTOR INSIDE A COMPONENT */}
           <ReactMapGL
+            {...viewport}
             mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-            {...this.state.viewport}
-            onViewportChange={viewport => this.setState({ viewport })}
+            width="100%"
+            onViewportChange={this._onViewportChange}
           />
         </Col>
       </Row>
