b07ccef64419baae91ecdef1e915c4cdef44edbd
Christophe Begue <Christophe@MacBook-Pro-de-MACBOOK-5.local>
2019-02-28 12:12:42 +0100

    fixing merges

--- src/App.js
+++ src/App.js
@@ -5,20 +5,14 @@ import { Switch, Route } from "react-router-dom";
 import Navigation from "./components/Navigation.js";
 import IsEmergency from "./components/IsEmergency.js";
 import SpeedDial from "./components/SpeedDial.js";
-import FormFill from "./components/FormFill.js";
 import Results from "./components/Results.js";
 import MapWrapper from "./components/MapWrapper.js";
 import NotFound from "./components/NotFound.js";
 //Css/styling
 import "./App.scss";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { Switch, Route } from "react-router-dom";
-import Navigation from "./components/Navigation";
-import Home from "./components/Home";
-import Template from "./components/Template";
-import SingleMap from "./components/SingleMap";
-import Questions from "./components/Questions";
-import Container from "react-bootstrap/Container";
+import Questions from "./components/Questions.js";
+import Container from "react-bootstrap/Container.js";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 require("dotenv").config();
@@ -40,6 +34,7 @@ class App extends Component {
 
             <Route path="/404" component={NotFound} />
           </Switch>
+
           <footer className="fixed-bottom">
             <SpeedDial />
           </footer>
