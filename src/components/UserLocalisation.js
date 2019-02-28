470e84d24787d30355eeac13e2ed38b5bfbc43cf
Christophe Begue <Christophe@MacBook-Pro-de-MACBOOK-5.local>
2019-02-28 18:09:38 +0100

    pluggin end of form to map

--- src/components/UserLocalisation.js
+++ src/components/UserLocalisation.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
+import Jumbotron from "react-bootstrap/Button";
+
 import { Link } from "react-router-dom";
 
 class UserLocalisation extends Component {
   state = {};
+
   render() {
     return (
       <div className="UserLocalisation">
-        <Link to="/results">Vers les resultats</Link>
+        <h3>Ou se trouve le patient ?</h3>
+        <Jumbotron>
+          <h3>Entrez votre adresse.</h3>
+
+          <p>
+            <Button variant="primary">Vers mes résultats</Button>
+          </p>
+        </Jumbotron>
+        <Link to="/map">vers les résultats</Link>
       </div>
     );
   }
