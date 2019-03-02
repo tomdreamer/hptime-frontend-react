cdaac91eab48530b5d50d681fdd0fdec25a28743
Samuel Bouaroua <bouaroua.samuel@gmail.com>
2019-03-02 10:08:00 +0100

    refactoring homepage popup

--- src/components/IsEmergency.js
+++ src/components/IsEmergency.js
@@ -1,32 +1,79 @@
 import React, { PureComponent } from "react";
-import "./IsEmergency.css";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { Link } from "react-router-dom";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import { LinkContainer } from "react-router-bootstrap";
+import "./IsEmergency.scss";
 
 class IsEmergency extends PureComponent {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      show: false
+    };
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  // show and hide modal
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
+  // open on page load
+  componentDidMount() {
+    this.handleShow();
+  }
+
   render() {
     return (
-      <section className="IsEmergency">
-        <Row className="justify-content-md-center">
-          <Col md="8">
-            <div className="shadow p-3 mb-5 bg-white rounded" role="alert">
-              <h4 className="alert-heading">!Attention!</h4>
-              <p>
+      <Row>
+        <Col>
+          {/* <Button variant="primary" onClick={this.handleShow}>
+            Launch demo modal
+          </Button> */}
+
+          <Modal
+            size="md"
+            aria-labelledby="contained-modal-title-vcenter"
+            show={this.state.show}
+            onHide={this.handleClose}
+          >
+            <Modal.Header>
+              <Modal.Title id="contained-modal-title-vcenter">
                 Est-ce une urgence vitale?
-                <br />
-                <br />
-                <a className="btn" href="tel:+3315">
-                  Yes
-                </a>{" "}
-                <Link className="btn" to={"/form"}>
-                  No
-                </Link>
+              </Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+              <p>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae.
               </p>
-            </div>
-          </Col>
-        </Row>
-      </section>
+            </Modal.Body>
+
+            <Modal.Footer>
+              <a href="tel:+3315">
+                <Button variant="danger">
+                  <i className="fas fa-phone" /> Appeller le SAMU (15)
+                </Button>
+              </a>
+
+              <LinkContainer to="/form">
+                <Button variant="primary">
+                  <i className="fas fa-arrow-right" /> Continuer
+                </Button>
+              </LinkContainer>
+            </Modal.Footer>
+          </Modal>
+        </Col>
+      </Row>
     );
   }
 }
