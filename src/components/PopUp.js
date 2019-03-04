46f0fc5bd7b15ce55f27d1d7303ccb4e0225a4cf
Christina Bailey <christinaibailey@live.com>
2019-03-04 18:56:31 +0100

    popup styling

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -17,13 +17,38 @@ class PopUp extends Component {
         anchor="top"
         onClose={this.props.onCloseClick}
       >
-        <p>
+        {/* <p>
           <b> {popupInfo.name}</b>
           <br />
           <a href="tel:+33{popupInfo.phoneNumber}">
             {popupInfo.phoneNumber}
-          </a>{" "}
-        </p>
+          </a>
+        </p> */}
+
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">{popupInfo.name}</h5>
+            <h6 class="card-subtitle mb-2">
+              <a href="tel:+33{popupInfo.phoneNumber}">
+                {popupInfo.phoneNumber}
+              </a>
+            </h6>
+            <p class="card-text">
+              {popupInfo.streetNumber} {popupInfo.roadType}{" "}
+              {popupInfo.streetName}
+              <br />
+              {popupInfo.city}
+              {popupInfo.country}
+              {popupInfo.zipCode}
+            </p>
+            <a href="#" class="card-link">
+              Route Me
+            </a>
+            <a href={popupInfo.urlToPlan} class="card-link">
+              Access Plan
+            </a>
+          </div>
+        </div>
       </Popup>
     );
   }
