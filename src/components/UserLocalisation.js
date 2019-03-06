b1afec6a6fcc64cb0b1539ccbeaae69ae65ef310
Samuel Bouaroua <bouaroua.samuel@gmail.com>
2019-03-06 13:17:41 +0100

    search form polishing

--- src/components/UserLocalisation.js
+++ src/components/UserLocalisation.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import LocationSearchInput from "./LocationSearchInput.js";
@@ -7,33 +8,42 @@ import "./UserLocalisation.scss";
 class UserLocalisation extends Component {
   render() {
     return (
-      <section className="text-center">
-        <div className="bg-directions" />
+      <section className="text-center ">
+        <div className="bg-directions m-4" />
         <p className="lead my-4">Trouvez des soins proches de vous.</p>
-
-        <Col md={{ span: 12 }}>
-          <div className="bg-light shadow-sm border rounded p-2 w-50 mx-auto">
-            <LocationSearchInput
-              onGeolocation={this.props.onGeolocation}
-              nextStep={event => this.props.nextStep(event)}
-            />
-          </div>
-          <Form className="p-2 small w-50 mx-auto text-left">
-            <div key="hint-checkbox" className="hint-checkbox m-2 px-2">
-              <label>
-                <Form.Check
-                  type="checkbox"
-                  id="prescription"
-                  className="d-inline mr-2"
-                />
-                <span className="text-muted">
-                  C'est pour renouveller une ordonnance ?{" "}
-                  <span className="text-dark">Cliquez ici !</span>
-                </span>
-              </label>
-            </div>
-          </Form>
-        </Col>
+        <Row>
+          <Col
+            sm={{ span: 12 }}
+            className=" px-5 d-flex justify-content-center"
+          >
+            <Col className="bg-light shadow-sm border rounded p-2 col-md-6 col-xs-12">
+              <LocationSearchInput
+                onGeolocation={this.props.onGeolocation}
+                nextStep={event => this.props.nextStep(event)}
+              />
+            </Col>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={{ span: 12 }} className="d-flex justify-content-center">
+            {/* prescription checkbox */}
+            <Form className="p-2 small w-50 mx-auto text-left">
+              <div key="hint-checkbox" className="hint-checkbox m-2 px-2">
+                <label>
+                  <Form.Check
+                    type="checkbox"
+                    id="prescription"
+                    className="d-inline mr-2"
+                  />
+                  <span className="text-muted">
+                    C'est pour renouveller une ordonnance ?{" "}
+                    <span className="text-secondary">Cliquez ici !</span>
+                  </span>
+                </label>
+              </div>
+            </Form>
+          </Col>
+        </Row>
       </section>
     );
   }
