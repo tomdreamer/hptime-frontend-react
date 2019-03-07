d833f7dd123b8176fe2ce08d7729e1f0da8cdfd0
Samuel Bouaroua <bouaroua.samuel@gmail.com>
2019-03-07 23:04:23 +0100

    dial fix

--- src/components/SpeedDial.js
+++ src/components/SpeedDial.js
@@ -4,8 +4,9 @@ import "./SpeedDial.css";
 class SpeedDial extends Component {
   render() {
     return (
-      <a href="tel:15" class="float">
-        <i class="fa fa-phone my-float" aria-hidden="true" /> 15
+      <a href="tel:15" className="float bg-danger text-white rounded-circle">
+        <i className="fa fa-phone my-float fa-sm" aria-hidden="true" />{" "}
+        <span className="dial-text">15</span>
       </a>
     );
   }
