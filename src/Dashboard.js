191edcdd3ce1b386ec16415b3923e58adc1a7f6b
Christina Bailey <christinaibailey@live.com>
2019-03-08 11:49:51 +0100

    remove ugly sidebar and added items to sign in nav bar

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -9,23 +9,13 @@ class Dashboard extends Component {
       <section className="Dashboard">
         <div className="container-fluid">
           <div className="row">
-            <div className="col-sm-3 col-md-2 sidebar">
-              <div className="vertical-menu">
-                <a href="#0" className="active">
-                  Home
-                </a>
-                <a href="#0">Link 1</a>
-                <a href="#0">Link 2</a>
-                <a href="#0">Link 3</a>
-                <a href="#0">Link 4</a>
-              </div>
-            </div>
+         
             <div className="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-              <h1 className="page-header">Dashboard</h1>
+              <h1 className="page-header">Yard Dashboard</h1>
 
               <div className="row map" />
 
-              <h2 className="sub-header">Section title</h2>
+              <h2 className="sub-header">Repertoire des Hopitaux </h2>
               <div className="table-responsive">
                 <table className="table table-striped">
                   <thead>
